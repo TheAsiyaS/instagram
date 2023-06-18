db00edbb29224aeb60d8a2810293737d2dbdc81b
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-06-18 10:07:32 +0530

    Notification screen ui completed

--- lib/Presenation/Notification/Notification_screen.dart
+++ lib/Presenation/Notification/Notification_screen.dart
@@ -19,51 +19,51 @@ class NotificationScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // const Text(
-            //   'Today',
-            //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            // ),
-            // Container(
-            //   height: MediaQuery.of(context).size.height / 1.34,
-            //   width: double.infinity,
-            //   child: ListView.separated(
-            //       itemBuilder: (context, index) {
-            //         return Row(
-            //           children: [
-            //             Expanded(
-            //               child: ListTile(
-            //                 leading: const CircleAvatar(
-            //                   radius: 25,
-            //                   backgroundImage: NetworkImage(
-            //                       'https://cdn.wallpapersafari.com/31/2/5SX1m0.jpg'),
-            //                 ),
-            //                 title: Text('Username$index   like your Post'),
-            //               ),
-            //             ),
-            //             Text(
-            //               '$index d',
-            //               style: const TextStyle(color: kGrey),
-            //             ),
-            //             SizedBox(
-            //               width: MediaQuery.of(context).size.width / 13,
-            //             ),
-            //             Container(
-            //               height: 50,
-            //               width: 50,
-            //               decoration: const BoxDecoration(
-            //                   image: DecorationImage(
-            //                       image: NetworkImage(
-            //                           'https://thumbs.dreamstime.com/b/scenic-view-moraine-lake-mountain-range-sunset-landscape-canadian-rocky-mountains-49666349.jpg'),
-            //                       fit: BoxFit.cover)),
-            //             )
-            //           ],
-            //         );
-            //       },
-            //       separatorBuilder: (context, index) {
-            //         return h20;
-            //       },
-            //       itemCount: 20),
-            // )
+            const Text(
+              'Today',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 1.34,
+              width: double.infinity,
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: ListTile(
+                            leading: const CircleAvatar(
+                              radius: 25,
+                              backgroundImage: NetworkImage(
+                                  'https://cdn.wallpapersafari.com/31/2/5SX1m0.jpg'),
+                            ),
+                            title: Text('Username$index   like your Post'),
+                          ),
+                        ),
+                        Text(
+                          '$index d',
+                          style: const TextStyle(color: kGrey),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 13,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://thumbs.dreamstime.com/b/scenic-view-moraine-lake-mountain-range-sunset-landscape-canadian-rocky-mountains-49666349.jpg'),
+                                  fit: BoxFit.cover)),
+                        )
+                      ],
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return h20;
+                  },
+                  itemCount: 20),
+            )
           ],
         ),
       )),
