317d910270e7bb9cad753dd1de2b5d9e2d61b0ab
TheAsiyaS <theasiya.s@gmail.com>
2025-06-08 12:49:08 +0530

    feat: new state managed home screen

---
+++ lib/Presenation/Home/NewHomePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Newhomepage extends StatefulWidget {
+  const Newhomepage({super.key});
+
+  @override
+  State<Newhomepage> createState() => _NewhomepageState();
+}
+
+class _NewhomepageState extends State<Newhomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
